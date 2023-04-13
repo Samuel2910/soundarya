503b037c8d61d700d969447cc3193482f12fdb65
Samuel Thomas <76246091+Samuel2910@users.noreply.github.com>
2023-04-13 23:54:20 +0530

    Update nlp.py

--- nlp.py
+++ nlp.py
@@ -16,6 +16,7 @@ import pymysql
 from courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
 import pafy
 import plotly.express as px
+import nltk
 
 def fetch_yt_video(link):
     video = pafy.new(link)
